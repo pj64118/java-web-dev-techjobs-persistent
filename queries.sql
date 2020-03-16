a6b1f576cc01bf4d10456a1c4eca631e5bae4908
Patricia Jameson <pjameson1@kc.rr.com>
2020-03-16 09:13:18 -0500

    Completed assignment

--- queries.sql
+++ queries.sql
@@ -1,7 +1,23 @@
 ## Part 1: Test it with SQL
+SELECT COLUMN_NAME, DATA_TYPE
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'job'
+ORDER BY ORDINAL_POSITION;
 
 ## Part 2: Test it with SQL
+SELECT name
+FROM employer
+WHERE location LIKE '%St%Louis%';
 
 ## Part 3: Test it with SQL
+DROP TABLE job;
 
 ## Part 4: Test it with SQL
+SELECT s.name, s.description
+FROM skill s
+WHERE s.name IS NOT NULL
+  AND (s.id in
+		( SELECT DISTINCT skills_id
+			FROM job_skills js
+			WHERE js.jobs_id IS NOT NULL))
+ORDER BY 1;
